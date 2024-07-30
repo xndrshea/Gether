d457197e559c57f81c3702f66397e0cb38148e81
Alexander Shea <alexandershea@MacBook-Pro-7.local>
2024-07-30 23:28:44 +0200

    MUCH BETTER

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+    console.error("MongoDB URI is not defined in the environment variables.");
+    process.exit(1);
+}
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1);
+});
+
+app.get('/', async (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
